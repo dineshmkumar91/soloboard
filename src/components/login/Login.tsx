a8519a7c1b64021289a22c3efa608bb456b5cde6
dineshmkumar <dineshmkumar91@gmail.com>
2024-05-25 11:19:43 +0530

    Soloboard : Initial Commit

---
+++ src/components/login/Login.tsx
@@ -0,0 +1,31 @@
+import './login.css';
+
+const Login = () => {
+  return (
+  <>
+  <div className="wrapper">
+    <form>
+        <h1>Login</h1>
+        <div className="input-box">
+            <input type="text" placeholder="Username" required/>
+            <i className='bx bx-user'></i>
+        </div>
+        <div className="input-box">
+            <input type="password" placeholder="Password" required/>
+            <i className='bx bx-lock-alt'></i>
+        </div>
+        <div className="remember-forgot">
+            <label><input type="checkbox"/> Remember Me</label>
+            <a href="#">Forgot password?</a>
+        </div>
+        <button type="button" className="btn">Login</button>
+        <div className="register-link">
+            <p>Don't have an account? <a href="#">Register</a></p>
+        </div>
+    </form>
+  </div>
+  </>
+  )
+}
+
+export default Login
